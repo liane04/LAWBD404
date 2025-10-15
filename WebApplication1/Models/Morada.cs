366365ea4422eecbe538dcd7d19744d0b7a29d0d
Bruno Alves <153294873+BrunoAlvees@users.noreply.github.com>
2025-10-15 12:17:12 +0100

    classes

---
+++ WebApplication1/Models/Morada.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Morada
+    {
+        
+        [Key]
+        public int UtilizadorId { get; set; }
+
+        [Required, StringLength(160)]
+        public string Descricao { get; set; } = null!;
+
+        [Required, StringLength(8)]
+        [RegularExpression(@"^\d{4}-\d{3}$", ErrorMessage = "Formato 0000-000")]
+        public string CodigoPostal { get; set; } = null!;
+
+        [Required, StringLength(80)]
+        public string Localidade { get; set; } = null!;
+
+        public Utilizador Utilizador { get; set; } = null!;
+    }
+}
---
+++ WebApplication1/Models/Utilizador.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public abstract class Utilizador
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O username é obrigatório")]
+        [StringLength(60, MinimumLength = 3)]
+        public string Username { get; set; } = null!;
+
+        [Required(ErrorMessage = "O email é obrigatório")]
+        [EmailAddress(ErrorMessage = "Email inválido")]
+        [StringLength(254)]
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "O nome é obrigatório")]
+        [StringLength(120)]
+        public string Nome { get; set; } = null!;
+
+        [Required(ErrorMessage = "A password é obrigatória")]
+        [StringLength(255)]
+        public string PasswordHash { get; set; } = null!;
+
+        [StringLength(30)]
+        public string? Estado { get; set; }  // ex.: "ativo", "bloqueado"
+    }
+
+
