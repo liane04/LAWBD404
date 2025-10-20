92110337c8dc2995d3c1ea463a2f57774a1373a0
Braz55 <pm7703125@gmail.com>
2025-10-20 21:26:35 +0100

    views + controller do anuncio

---
+++ Marketplace/Controllers/AnunciosController.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Marketplace.Data;
+using Marketplace.Models;
+
+namespace Marketplace.Controllers
+{
+    public class AnunciosController : Controller
+    {
+        private readonly MarketplaceContext _context;
+
+        public AnunciosController(MarketplaceContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Anuncios
+        public async Task<IActionResult> Index()
+        {
+            var marketplaceContext = _context.Anuncio.Include(a => a.Categoria).Include(a => a.Combustivel).Include(a => a.Marca).Include(a => a.Modelo).Include(a => a.Tipo).Include(a => a.Vendedor);
+            return View(await marketplaceContext.ToListAsync());
+        }
+
+        // GET: Anuncios/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var anuncio = await _context.Anuncio
+                .Include(a => a.Categoria)
+                .Include(a => a.Combustivel)
+                .Include(a => a.Marca)
+                .Include(a => a.Modelo)
+                .Include(a => a.Tipo)
+                .Include(a => a.Vendedor)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (anuncio == null)
+            {
+                return NotFound();
+            }
+
+            return View(anuncio);
+        }
+
+        // GET: Anuncios/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoriaId"] = new SelectList(_context.Set<Categoria>(), "Id", "Nome");
+            ViewData["CombustivelId"] = new SelectList(_context.Set<Combustivel>(), "Id", "Tipo");
+            ViewData["MarcaId"] = new SelectList(_context.Set<Marca>(), "Id", "Nome");
+            ViewData["ModeloId"] = new SelectList(_context.Set<Modelo>(), "Id", "Nome");
+            ViewData["TipoId"] = new SelectList(_context.Set<Tipo>(), "Id", "Nome");
+            ViewData["VendedorId"] = new SelectList(_context.Set<Vendedor>(), "Id", "Discriminator");
+            return View();
+        }
+
+        // POST: Anuncios/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Preco,Ano,Cor,Descricao,Quilometragem,Titulo,Caixa,VendedorId,MarcaId,ModeloId,CategoriaId,CombustivelId,TipoId")] Anuncio anuncio)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(anuncio);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoriaId"] = new SelectList(_context.Set<Categoria>(), "Id", "Nome", anuncio.CategoriaId);
+            ViewData["CombustivelId"] = new SelectList(_context.Set<Combustivel>(), "Id", "Tipo", anuncio.CombustivelId);
+            ViewData["MarcaId"] = new SelectList(_context.Set<Marca>(), "Id", "Nome", anuncio.MarcaId);
+            ViewData["ModeloId"] = new SelectList(_context.Set<Modelo>(), "Id", "Nome", anuncio.ModeloId);
+            ViewData["TipoId"] = new SelectList(_context.Set<Tipo>(), "Id", "Nome", anuncio.TipoId);
+            ViewData["VendedorId"] = new SelectList(_context.Set<Vendedor>(), "Id", "Discriminator", anuncio.VendedorId);
+            return View(anuncio);
+        }
+
+        // GET: Anuncios/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var anuncio = await _context.Anuncio.FindAsync(id);
+            if (anuncio == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoriaId"] = new SelectList(_context.Set<Categoria>(), "Id", "Nome", anuncio.CategoriaId);
+            ViewData["CombustivelId"] = new SelectList(_context.Set<Combustivel>(), "Id", "Tipo", anuncio.CombustivelId);
+            ViewData["MarcaId"] = new SelectList(_context.Set<Marca>(), "Id", "Nome", anuncio.MarcaId);
+            ViewData["ModeloId"] = new SelectList(_context.Set<Modelo>(), "Id", "Nome", anuncio.ModeloId);
+            ViewData["TipoId"] = new SelectList(_context.Set<Tipo>(), "Id", "Nome", anuncio.TipoId);
+            ViewData["VendedorId"] = new SelectList(_context.Set<Vendedor>(), "Id", "Discriminator", anuncio.VendedorId);
+            return View(anuncio);
+        }
+
+        // POST: Anuncios/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Preco,Ano,Cor,Descricao,Quilometragem,Titulo,Caixa,VendedorId,MarcaId,ModeloId,CategoriaId,CombustivelId,TipoId")] Anuncio anuncio)
+        {
+            if (id != anuncio.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(anuncio);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AnuncioExists(anuncio.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoriaId"] = new SelectList(_context.Set<Categoria>(), "Id", "Nome", anuncio.CategoriaId);
+            ViewData["CombustivelId"] = new SelectList(_context.Set<Combustivel>(), "Id", "Tipo", anuncio.CombustivelId);
+            ViewData["MarcaId"] = new SelectList(_context.Set<Marca>(), "Id", "Nome", anuncio.MarcaId);
+            ViewData["ModeloId"] = new SelectList(_context.Set<Modelo>(), "Id", "Nome", anuncio.ModeloId);
+            ViewData["TipoId"] = new SelectList(_context.Set<Tipo>(), "Id", "Nome", anuncio.TipoId);
+            ViewData["VendedorId"] = new SelectList(_context.Set<Vendedor>(), "Id", "Discriminator", anuncio.VendedorId);
+            return View(anuncio);
+        }
+
+        // GET: Anuncios/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var anuncio = await _context.Anuncio
+                .Include(a => a.Categoria)
+                .Include(a => a.Combustivel)
+                .Include(a => a.Marca)
+                .Include(a => a.Modelo)
+                .Include(a => a.Tipo)
+                .Include(a => a.Vendedor)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (anuncio == null)
+            {
+                return NotFound();
+            }
+
+            return View(anuncio);
+        }
+
+        // POST: Anuncios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var anuncio = await _context.Anuncio.FindAsync(id);
+            if (anuncio != null)
+            {
+                _context.Anuncio.Remove(anuncio);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AnuncioExists(int id)
+        {
+            return _context.Anuncio.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Marketplace/Data/MarketplaceContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Marketplace.Models;
+
+namespace Marketplace.Data
+{
+    public class MarketplaceContext : DbContext
+    {
+        public MarketplaceContext (DbContextOptions<MarketplaceContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Marketplace.Models.Anuncio> Anuncio { get; set; } = default!;
+    }
+}
