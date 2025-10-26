bcf243a8eb3395817ac6d3597a51f0747e25f439
Bruno Alves <153294873+BrunoAlvees@users.noreply.github.com>
2025-10-26 23:01:24 +0000

    qf

--- Marketplace/Models/Anuncio.cs
+++ Marketplace/Models/Anuncio.cs
@@ -1,6 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace Marketplace.Models
 {
@@ -28,6 +27,17 @@ namespace Marketplace.Models
         [StringLength(50)]
         public string? Caixa { get; set; }
 
+        [StringLength(100)]
+        public string? Localizacao { get; set; }
+
+        public int? Portas { get; set; }
+
+        public int? Lugares { get; set; }
+
+        public int? Potencia { get; set; } // em cv
+
+        public int? Cilindrada { get; set; } // em cm³
+
         // Valor do sinal exigido para reserva
         [Column("Valor_sinal", TypeName = "decimal(10,2)")]
         public decimal ValorSinal { get; set; } = 0m;
@@ -36,6 +46,7 @@ namespace Marketplace.Models
         [Column("n_visualizacoes")]
         public int NVisualizacoes { get; set; } = 0;
 
+        // Relações com outras entidades
         public int VendedorId { get; set; }
         [ForeignKey("VendedorId")]
         public Vendedor Vendedor { get; set; } = null!;
@@ -63,11 +74,9 @@ namespace Marketplace.Models
         public ICollection<Imagem> Imagens { get; set; } = new List<Imagem>();
         public ICollection<Reserva> Reservas { get; set; } = new List<Reserva>();
         public ICollection<Compra> Compras { get; set; } = new List<Compra>();
-
         public ICollection<Conversa> Conversas { get; set; } = new List<Conversa>();
         public ICollection<DenunciaAnuncio> Denuncias { get; set; } = new List<DenunciaAnuncio>();
         public ICollection<AcaoAnuncio> AcoesAnuncio { get; set; } = new List<AcaoAnuncio>();
         public ICollection<AnuncioExtra> AnuncioExtras { get; set; } = new List<AnuncioExtra>();
     }
 }
-
